3023a028d0d3132f8c9c3993df6ad9fb781e2b61
Filipe Teixeira <filipebarrosteixeira98@gmail.com>
2023-09-27 15:41:48 -0300

    chore: created aside & form elements structure containing @shadcn-ui library components such as select/textarea/slider and other ones

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,17 @@
-import { Github } from 'lucide-react'
+import { FileVideo, Github, Upload, Wand2 } from 'lucide-react'
+import { Label } from '@radix-ui/react-label'
 
 import { Button } from './components/ui/button'
 import { Separator } from './components/ui/separator'
 import { Textarea } from './components/ui/textarea'
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from './components/ui/select'
+import { Slider } from './components/ui/slider'
 
 export function App() {
   return (
@@ -39,7 +48,81 @@ export function App() {
             prompt to add selected video transcription content.
           </p>
         </div>
-        <aside className="w-80"></aside>
+        <aside className="w-80 space-y-6">
+          <form className="space-y-6">
+            <label
+              htmlFor="video"
+              className="border flex rounded-md aspect-video cursor-pointer border-dashed text-sm flex-col gap-2 items-center justify-center text-muted-foreground hover:bg-primary/5"
+            >
+              <FileVideo className="w-4 h-4" />
+              Pick one video
+            </label>
+            <input
+              type="file"
+              id="video"
+              accept="video/mp4"
+              className="sr-only"
+            />
+            <Separator />
+            <div className="space-y-2">
+              <Label htmlFor="transcription_prompt">Transcription Prompt</Label>
+              <Textarea
+                id="transcription_prompt"
+                className="h-20 leading-relaxed resize-none"
+                placeholder="Include keywords mentioned in the video separated by commas (,)"
+              />
+            </div>
+            <Button type="submit" className="w-full">
+              Load video
+              <Upload className="w-4 h-4 ml-2" />
+            </Button>
+          </form>
+          <Separator />
+          <form className="space-y-6">
+            <div className="space-y-2">
+              <Label>Prompt</Label>
+              <Select>
+                <SelectTrigger>
+                  <SelectValue placeholder="Select a prompt..." />
+                </SelectTrigger>
+                <SelectContent>
+                  <SelectItem value="title">YouTube title</SelectItem>
+                  <SelectItem value="description">
+                    YouTube description
+                  </SelectItem>
+                </SelectContent>
+              </Select>
+            </div>
+            <div className="space-y-2">
+              <Label>Model</Label>
+              <Select disabled defaultValue="gpt3.5">
+                <SelectTrigger>
+                  <SelectValue />
+                </SelectTrigger>
+                <SelectContent>
+                  <SelectItem value="gpt3.5">GPT 3.5-turbo 16k</SelectItem>
+                </SelectContent>
+              </Select>
+              <span className="block text-xs text-muted-foreground italic">
+                You will be able to customize this option soon
+              </span>
+            </div>
+            <Separator />
+            <div className="space-y-4">
+              <Label>Temperature</Label>
+              <Slider min={0} max={1} step={0.1} />
+              <span className="block text-xs text-muted-foreground italic leading-relaxed">
+                higher values tend to make the result more creative and with
+                possible errors.
+              </span>
+            </div>
+            <Separator />
+            <Button type="submit" className="w-full">
+              Execute
+              <Wand2 className="w-4 h-4 ml-2" />
+            </Button>
+          </form>
+        </aside>
       </main>
     </div>
   )
